2e52686b9dd37eda0216e7476fa595d47ab52d20
sonny_maan@yahoo.com <ubuntu@ip-172-31-17-147.eu-west-1.compute.internal>
2019-07-26 09:53:56 +0000

    Added errors for no bikes available and docking station full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,13 @@
 class DockingStation
-
-    def release_bike
-      Bike.new
-    end
-    
-    def dock(bike)
-      @bike = bike
-    end
-    
     attr_reader :bike
+    def release_bike
+      fail 'No bikes available' unless @bike
+      @bike
+    end
     
+def dock(bike)
+    fail 'Docking station full' if @bike
+    @bike = bike
+end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,33 @@ require "docking_station"
 require "bike"
 
 describe DockingStation do
+  
     it { expect(DockingStation.new).to respond_to :release_bike }
-    it { expect(DockingStation.new.release_bike).to be_a(Bike) }
     
     it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-  
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
     it { is_expected.to respond_to(:bike) }
+    
+    describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    
+    describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+    
+    describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+end
+end
+end
 end
